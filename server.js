ae99374f0808141ffefaeb447992f3b5e817dbf5
Ramesh K <rmshk841@gmail.com>
2024-10-24 13:48:37 +0530

    Added React Server Components example

---
+++ server.js
@@ -0,0 +1,26 @@
+import http from 'http';
+import fs from 'fs';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+
+import App from './client/App.jsx'; // transpiling with babel is required
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+
+  res.end(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="UTF-8">
+        <title>React SSR with Client-Side Hydration</title>
+      </head>
+      <body>
+        <div id="root">${renderToString(React.createElement(App))}</div>
+        <script src="/client.js"></script>
+      </body>
+    </html>
+    `);
+}).listen(3000, () => {
+    console.log(`Server is running on http://localhost:${PORT}/`);
+});
